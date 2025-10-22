a567739400bf4e14da5c3b5bcc56320aacb13739
loneandhungry <mstejaswani@gmail.com>
2025-10-22 18:39:28 +0530

    wow

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -10,6 +10,8 @@ function getLength(l){
     else {prompt = 4 ; return {max_length: 240, min_length: 140}}
 }
 
+/*
+
 export async function  generateSummary(text,length){
      if(!text || text.trim().length < 30){
         return `${text} (Too short to summarize bro!)`;
@@ -53,5 +55,57 @@ export async function  generateSummary(text,length){
     } catch (err) {
     console.error("Hugging Face Generate Summary API Error:", err.message);
     return "Error in generating summary. Please try again.";
+
+}
+}
+*/
+
+
+
+export async function  generateSummary(text,length) {
+  if (!text || text.trim().length < 30) {
+    return `${text} (Too short to summarize bro!)`;
   }
-    }
+
+ const { max_length, min_length } = getLength(length);
+const data =({
+    messages: [
+        {
+            role: "user",
+            content: `Summarize the following text concisely in ${max_length} characters. 
+                Only include information explicitly present in the text. 
+                Do NOT add anything extra, do NOT infer, do NOT assume. 
+               If the text is too short to summarize, just return it as-is. 
+                 :\n${text}`,
+        },
+    ],
+    model: "meta-llama/Llama-3.2-1B-Instruct:novita",
+    max_tokens: max_length,
+})
+
+try{
+	const response = await fetch(
+		"https://router.huggingface.co/v1/chat/completions",
+		{
+			headers: {
+				Authorization:`Bearer ${KEY}`,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+     const summary = result?.choices?.[0]?.message?.content;  
+ if(!summary) throw new Error("Inavlid response from hugging face"); 
+	return summary;
+} catch(error){
+  console.log(error.message);
+  return "Error in Generating Summary. Please try again."
+}
+
+}
+
+
+
+  
